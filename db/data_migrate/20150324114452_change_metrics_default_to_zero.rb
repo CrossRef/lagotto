9f55de1356562a1780a706af7377cf1b3bab581d
Joe Wass <jwass@crossref.org>
2016-03-31 17:55:51 +0100

    Removed data migrations for Event.

--- db/data_migrate/20150324114452_change_metrics_default_to_zero.rb
+++ db/data_migrate/20150324114452_change_metrics_default_to_zero.rb
@@ -1,8 +1,5 @@
 class ChangeMetricsDefaultToZero < ActiveRecord::Migration
   def up
-    Event.update_all(pdf: 0, html: 0, readers: 0, comments: 0, likes: 0, extra: nil)
-    Month.update_all(pdf: 0, html: 0, readers: 0, comments: 0, likes: 0)
-    Day.update_all(pdf: 0, html: 0, readers: 0, comments: 0, likes: 0)
     Change.update_all(pdf: 0, html: 0, total: 0)
   end
 
--- db/data_migrate/20150607195120_remove_empty_event_rows.rb
+++ db/data_migrate/20150607195120_remove_empty_event_rows.rb
@@ -1,8 +1,5 @@
 class RemoveEmptyEventRows < ActiveRecord::Migration
   def up
-    Event.where(total: 0).delete_all
-    Month.where('event_id NOT IN (SELECT id FROM events)').delete_all
-    Day.where('event_id NOT IN (SELECT id FROM events)').delete_all
   end
 
   def down
